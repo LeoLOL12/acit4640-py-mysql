c0c25b238c6b04ffc7182c1df8762551ebcbb3bc
LeoLi <71402271+LeoLOL12@users.noreply.github.com>
2022-11-21 12:30:28 -0800

    add sleep

--- db_insert/insert.sh
+++ db_insert/insert.sh
@@ -1,4 +1,5 @@
 # mysql -h ${MYSQL_HOST} -P 3306 -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} < insert.sql
+sleep 5
 mysql -h ${MYSQL_HOST} -P 3306 -u ${MYSQL_USER} -p${MYSQL_PASSWORD} \
 -e "INSERT INTO item (name, bcit_id) VALUES ('${STUDENT_NAME}', '${BCIT_ID}');" \
 ${MYSQL_DATABASE}
